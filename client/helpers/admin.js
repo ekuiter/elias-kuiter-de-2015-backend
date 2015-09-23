a528c1597a5ecdca7c0ccd529b38183bc34772a6
Elias Kuiter <info@elias-kuiter.de>
2015-09-23 19:23:59 +0200

    Database fix

--- client/helpers/admin.js
+++ client/helpers/admin.js
@@ -58,12 +58,20 @@ Template.adminPicturesActionsColumn.events({
   }
 });
 
+function fetchDatabaseStats() {
+  Meteor.call("databaseStats", function(err, res) {
+    Session.set("databaseStats", res);
+  });
+}
+
+Template.adminDatabaseWidget.onCreated(fetchDatabaseStats);
+
 Template.adminDatabaseWidget.helpers({
-  stats: ReactivePromise(function() {
-    return Meteor.promise("databaseStats");
-  }, {}),
+  stats: function() {
+    return Session.get("databaseStats");
+  },
   formatBytes: function(bytes) {
-    if (!bytes) return "&nbsp;";
+    if (!bytes) return "...";
     return (bytes / 1024 / 1024).toFixed(1);
   },
   maxSize: function() {
